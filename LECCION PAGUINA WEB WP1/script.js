469e0e569936535364ef12b221edd45ea83544b2
washington rene panchi panchi <renepanchi@hotmail.com>
2024-09-26 13:01:52 -0500

    Add files via upload

---
+++ LECCION PAGUINA WEB WP1/script.js
@@ -0,0 +1,24 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    if (index >= slides.length) currentSlide = 0;
+    if (index < 0) currentSlide = slides.length - 1;
+
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === currentSlide) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function changeSlide(direction) {
+    currentSlide += direction;
+    showSlide(currentSlide);
+}
+
+// Inicializar el slider
+showSlide(currentSlide);
+
+
